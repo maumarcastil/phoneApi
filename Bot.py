d6a8bb18c53f6f63dd278b3bb9162924222b860f
unknown <cadicossamm@gmail.com>
2021-02-20 08:36:42 -0500

    automatizacion completa

--- Bot.py
+++ Bot.py
@@ -3,7 +3,35 @@ from selenium.webdriver.firefox.options import Options
 import time
 import json
 
-import phone_api
+
+class Dispositivo(object):
+    def __init__(self, name, img):
+        self.name = name
+        self.img = img
+
+
+class Marca(object):
+    def __init__(self, name, logo):
+        self.brand = name
+        self.logo = logo
+        self.devices = []
+
+    def add_device(self, device):
+        self.devices.append(device)
+    
+    def to_JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+
+class Marcas(object):
+    def __init__(self):
+        self.brands = []
+
+    def add_brand(self, brand):
+        self.brands.append(brand)
+
+    def to_JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
 
 class Bot():
@@ -22,18 +50,72 @@ class Bot():
             "https://www.kimovil.com/es/todas-las-marcas-de-moviles")
 
         time.sleep(2)
-        print("Finalizo")
+        
+        
+    def get_all_devices_for_brands(self):
+        i = 0
+        brands = Marcas()
+        links_brands = list(self.browser.execute_script("return [...document.querySelectorAll('.navigation-list li.item')].map(x => x.lastChild.lastChild.href)"))
+        print("llego aca")
+        for x in links_brands:
+            if(i == 2):
+                break
+            self.browser.get(x)
+            time.sleep(5)
+            
+            print("llego aca x2")
+            
+            ##Get data(NameBrand and logo)
+            logo = self.browser.execute_script("return document.querySelector('.h-main .h-left img').src")
+            name_brand = self.browser.execute_script("return document.querySelector('.h-main .h-right h1').innerText")
+            name_brand = name_brand.replace("Comparar precios de móviles libres de ", "")
+            
+            print("llego aca x3")
+            
+            ##Create brand
+            brand = Marca(name_brand, logo)
+            
+            print("llego aca x4")
+            
+            ##Get url of all devices
+            links_devices = list(self.browser.execute_script("return [...document.querySelectorAll('.item.smartphone a')].map(x => x.href)"))
+            for y in links_devices:
+                self.browser.get(y)
+                time.sleep(2)
+                
+                ##Get nameDevice and imgs
+                imgs_device = self.browser.execute_script("return [...document.querySelectorAll('a.kigallery')].map(x => x.href.search('cdn-files.kimovil.com') != -1 ? x.href : false)")
+                name_device = self.browser.execute_script("return document.querySelector('.wrapper .title-group h1').lastChild.wholeText.trim()")
 
-    def get_all_links_brands(self):
-        links_brands = self.browser.execute_script(
-            "return [...document.querySelectorAll('.navigation-list li.item')].map(x => x.lastChild.lastChild.href)")
-        print(links_brands[0])
-
+                ##Created Device
+                print("Se creo el dispositivo: {}".format(name_device))
+                device = Dispositivo(name_device, imgs_device)
+                
+                ##Add device in brand
+                print("Se agregro el dispositivo")
+                brand.add_device(device)
+                
+            print("Agregando la marca")
+            brands.add_brand(brand)
+            i = i + 1
+            
+        print("Mostrando las marcas")
+        print(brands.to_JSON())
+            
+                
+                
+                
+                
+            
+            
 
 def Main():
     MyBot = Bot()
-    MyBot.get_all_links_brands()
+    MyBot.get_all_devices_for_brands()
+    print("Finalizo")
 
 
 if __name__ == '__main__':
     Main()
+    
+
--- phone_api.py
+++ phone_api.py
@@ -15,6 +15,9 @@ class Marca(object):
 
     def add_device(self, device):
         self.devices.append(device)
+    
+    def to_JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
 
 class Marcas(object):
@@ -29,46 +32,46 @@ class Marcas(object):
 
 
 
-data_marca = Marca(
-    "360", "https://cdn-files.kimovil.com/brands/0002/11/thumb_110250_brands_small.png")
+# data_marca = Marca(
+#     "360", "https://cdn-files.kimovil.com/brands/0002/11/thumb_110250_brands_small.png")
 
 
-nombre_dispositivo1 = "cell 1"
-fotos_dispositivo1 = ["https://cdn-files.kimovil.com/default/0002/55/thumb_154387_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154395_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154394_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154393_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154391_default_big.jpeg",
-                      "https://cdn-files.kimovil.com/default/0002/55/thumb_154392_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154390_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154389_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154388_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154396_default_big.jpeg"]
+# nombre_dispositivo1 = "cell 1"
+# fotos_dispositivo1 = ["https://cdn-files.kimovil.com/default/0002/55/thumb_154387_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154395_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154394_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154393_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154391_default_big.jpeg",
+#                       "https://cdn-files.kimovil.com/default/0002/55/thumb_154392_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154390_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154389_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154388_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154396_default_big.jpeg"]
 
-nombre_dispositivo2 = "cell 2"
-fotos_dispositivo2 = ["https://cdn-files.kimovil.com/default/0002/55/thumb_154387_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154395_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154394_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154393_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154391_default_big.jpeg",
-                      "https://cdn-files.kimovil.com/default/0002/55/thumb_154392_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154390_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154389_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154388_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154396_default_big.jpeg"]
+# nombre_dispositivo2 = "cell 2"
+# fotos_dispositivo2 = ["https://cdn-files.kimovil.com/default/0002/55/thumb_154387_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154395_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154394_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154393_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154391_default_big.jpeg",
+#                       "https://cdn-files.kimovil.com/default/0002/55/thumb_154392_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154390_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154389_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154388_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154396_default_big.jpeg"]
 
-nombre_dispositivo3 = "cell 3"
-fotos_dispositivo3 = ["https://cdn-files.kimovil.com/default/0002/55/thumb_154387_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154395_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154394_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154393_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154391_default_big.jpeg",
-                      "https://cdn-files.kimovil.com/default/0002/55/thumb_154392_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154390_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154389_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154388_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154396_default_big.jpeg"]
+# nombre_dispositivo3 = "cell 3"
+# fotos_dispositivo3 = ["https://cdn-files.kimovil.com/default/0002/55/thumb_154387_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154395_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154394_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154393_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154391_default_big.jpeg",
+#                       "https://cdn-files.kimovil.com/default/0002/55/thumb_154392_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154390_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154389_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154388_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154396_default_big.jpeg"]
 
-nombre_dispositivo4 = "cell 4"
-fotos_dispositivo4 = ["https://cdn-files.kimovil.com/default/0002/55/thumb_154387_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154395_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154394_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154393_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154391_default_big.jpeg",
-                      "https://cdn-files.kimovil.com/default/0002/55/thumb_154392_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154390_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154389_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154388_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154396_default_big.jpeg"]
+# nombre_dispositivo4 = "cell 4"
+# fotos_dispositivo4 = ["https://cdn-files.kimovil.com/default/0002/55/thumb_154387_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154395_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154394_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154393_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154391_default_big.jpeg",
+#                       "https://cdn-files.kimovil.com/default/0002/55/thumb_154392_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154390_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154389_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154388_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154396_default_big.jpeg"]
 
-nombre_dispositivo5 = "cell 5"
-fotos_dispositivo5 = ["https://cdn-files.kimovil.com/default/0002/55/thumb_154387_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154395_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154394_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154393_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154391_default_big.jpeg",
-                      "https://cdn-files.kimovil.com/default/0002/55/thumb_154392_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154390_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154389_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154388_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154396_default_big.jpeg"]
-
-
-data_dispositivo1 = Dispositivo(nombre_dispositivo1, fotos_dispositivo1)
-data_dispositivo2 = Dispositivo(nombre_dispositivo2, fotos_dispositivo2)
-data_dispositivo3 = Dispositivo(nombre_dispositivo3, fotos_dispositivo3)
-data_dispositivo4 = Dispositivo(nombre_dispositivo4, fotos_dispositivo4)
-data_dispositivo5 = Dispositivo(nombre_dispositivo5, fotos_dispositivo5)
+# nombre_dispositivo5 = "cell 5"
+# fotos_dispositivo5 = ["https://cdn-files.kimovil.com/default/0002/55/thumb_154387_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154395_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154394_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154393_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154391_default_big.jpeg",
+#                       "https://cdn-files.kimovil.com/default/0002/55/thumb_154392_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154390_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154389_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154388_default_big.jpeg", "https://cdn-files.kimovil.com/default/0002/55/thumb_154396_default_big.jpeg"]
 
 
-data_marca.add_device(data_dispositivo1)
-data_marca.add_device(data_dispositivo1)
-data_marca.add_device(data_dispositivo3)
-data_marca.add_device(data_dispositivo4)
-data_marca.add_device(data_dispositivo5)
+# data_dispositivo1 = Dispositivo(nombre_dispositivo1, fotos_dispositivo1)
+# data_dispositivo2 = Dispositivo(nombre_dispositivo2, fotos_dispositivo2)
+# data_dispositivo3 = Dispositivo(nombre_dispositivo3, fotos_dispositivo3)
+# data_dispositivo4 = Dispositivo(nombre_dispositivo4, fotos_dispositivo4)
+# data_dispositivo5 = Dispositivo(nombre_dispositivo5, fotos_dispositivo5)
 
 
-marcas = Marcas()
-marcas.add_brand(data_marca)
+# data_marca.add_device(data_dispositivo1)
+# data_marca.add_device(data_dispositivo1)
+# data_marca.add_device(data_dispositivo3)
+# data_marca.add_device(data_dispositivo4)
+# data_marca.add_device(data_dispositivo5)
 
-print(marcas.to_JSON())
+
+# marcas = Marcas()
+# marcas.add_brand(data_marca)
+
+# print(marcas.to_JSON())
